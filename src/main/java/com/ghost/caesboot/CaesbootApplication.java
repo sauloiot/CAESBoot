73e7820db6f4845c77e4f71baad245b508fac706
Saulo Tavares <saulo.11@hotmail.com>
2020-06-12 16:21:08 -0300

    first commit CAESBoot

---
+++ src/main/java/com/ghost/caesboot/CaesbootApplication.java
@@ -0,0 +1,13 @@
+package com.ghost.caesboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaesbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaesbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ghost/caesboot/CaesbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ghost.caesboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaesbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
