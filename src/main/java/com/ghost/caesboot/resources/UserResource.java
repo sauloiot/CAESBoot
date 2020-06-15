a2bc7c5d85c137956824fa9a4e530b4b7e06026d
Saulo Tavares <saulo.11@hotmail.com>
2020-06-15 16:12:30 -0300

    get user byId (findById)

--- src/main/java/com/ghost/caesboot/resources/UserResource.java
+++ src/main/java/com/ghost/caesboot/resources/UserResource.java
@@ -5,15 +5,13 @@ import com.ghost.caesboot.dto.UserDTO;
 import com.ghost.caesboot.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+
 
 @RestController
 @RequestMapping(value = "/users")
@@ -25,10 +23,16 @@ public class UserResource {
     //    @GetMapping ou
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<UserDTO>> findAll() {
-
         List<User> list = service.findAll();
         List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
-
         return ResponseEntity.ok().body(listDto);
     }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User obj = service.findById(id);
+         return ResponseEntity.ok().body(new UserDTO(obj));
+    }
+
+
 }
