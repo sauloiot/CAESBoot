d5af1dd73cc1487052279880fb253dc39edc5c02
Saulo Tavares <saulo.11@hotmail.com>
2020-06-18 19:16:34 -0300

    user posts reference

--- src/main/java/com/ghost/caesboot/config/Instantiation.java
+++ src/main/java/com/ghost/caesboot/config/Instantiation.java
@@ -39,8 +39,13 @@ public class Instantiation implements CommandLineRunner {
         Post post1 = new Post(null, sdf.parse("01/01/2016"), "Estou pronto !", "Estoooooooou prontoooooo, estoooooou prontooooo", new AuthorDTO(bobEsponja));
         Post post2 = new Post(null, sdf.parse("21/03/2018"), "anti vilão", "vamos derrotar todos os vilões deste universo",new AuthorDTO(bruceWayne));
         Post post3 = new Post(null, sdf.parse("17/06/2019"), "união", "unieremos a liga da justiça com os vingadores", new AuthorDTO(tonyStark));
+        Post post4 = new Post(null, sdf.parse("21/03/2018"), "martha", "for all martha mothers ",new AuthorDTO(bruceWayne));
 
 
-        postRepository.saveAll(Arrays.asList(post1,post2, post3));
+        postRepository.saveAll(Arrays.asList(post1,post2, post3, post4));
+
+        bruceWayne.getPosts().addAll(Arrays.asList(post2, post4));
+        userRepository.save(bruceWayne);
     }
+
 }
